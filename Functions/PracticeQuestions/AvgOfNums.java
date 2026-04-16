c1ba40f941c6a020c4a4c557e55f02031996c0dc
Farhana-Pathan <farhanapathan174@gmail.com>
2026-04-16 23:37:25 +0530

    Function Practice Questions

---
+++ Functions/PracticeQuestions/AvgOfNums.java
@@ -0,0 +1,15 @@
+package Core.Functions.PracticeQuestions;
+
+// Write a Java method to compute the averageof three numbers
+
+public class AvgOfNums {
+    
+    public static int CalcAvg(int a, int b, int c){
+        int avg = (a + b + c)/3;
+        return avg; 
+    }
+
+    public static void main(String[] args) {
+        System.out.println(CalcAvg(3, 4, 5));
+    }
+}
